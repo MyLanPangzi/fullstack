3125b834fdf7cfd4bbb81027f6b29b4cec398357
MyLanpangzi <1251723871@qq.com>
2019-05-04 22:25:55 +0800

    Thinking In React的小栗子 第二版

--- typescript/ts-web-react/filter-product-table/src/index.tsx
+++ typescript/ts-web-react/filter-product-table/src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
-import FilterProductTable from "./components/FilterableProductTable";
 import FilterableProductTable from "./components/FilterableProductTable";
 
 const PRODUCTS = [
