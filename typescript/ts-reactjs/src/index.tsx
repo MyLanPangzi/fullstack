6d732bd399a8c2baaaeae43be3753a3c5671a0df
MyLanpangzi <1251723871@qq.com>
2019-05-15 21:55:46 +0800

    调整tic-tac-toe运行

--- typescript/ts-reactjs/src/index.tsx
+++ typescript/ts-reactjs/src/index.tsx
@@ -4,6 +4,6 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 import {Game} from "./tic-tac-toe/Game";
 
-ReactDOM.render(<Game />, document.getElementById('root'));
+ReactDOM.render(<Game />, document.getElementById('tic-tac-toe'));
 
 serviceWorker.register();
